084ffc14da3557f93f986b2742a09b9cc6bdf53e
melissa <melquinonez9@gmail.com>
2023-05-31 18:02:27 -0500

    feat(backenf): some files to backend.

--- django-todo-react/backend/backend/settings.py
+++ django-todo-react/backend/backend/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://127.0.0.1:3000'
 ]
 
 ROOT_URLCONF = 'backend.urls'
